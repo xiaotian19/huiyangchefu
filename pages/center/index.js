c5ee1f42ed4aa66368a6f511fc27294978fb5c03
小天 <948993029@qq.com>
2020-06-17 18:56:23 +0800

    修复个人中心页点击失效

--- pages/center/index.js
+++ pages/center/index.js
@@ -79,8 +79,8 @@ Page({
    */
 
   navTapType(e) {
-
-    switch (e.detail) {
+    
+    switch (e.detail.name) {
       case '邀请车主':
         this.getCode("pages/index/index", '你正在邀请：车主');
         break
