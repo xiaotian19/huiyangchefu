a7b10c6b8c3cb27f6ddfae5affcb7053dd1852a4
小天 <948993029@qq.com>
2020-06-18 19:51:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ App({
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function (options) {
-    console.log(options)
     this.checkIn();
     // 缓存邀请人id
     if (Object.keys(options.query).length != 0) {
